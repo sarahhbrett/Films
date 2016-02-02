31e70120e4e91c2b2a910d5596d846a05e2ec14c
unknown <c13314646@BUS-1025-47.ict.ad.dit.ie>
2016-02-02 15:29:23 +0000

    Now have login/logout

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,11 @@ class Movie < ActiveRecord::Base
 	belongs_to :genre, dependent: :destroy
 	
 	validates :title, :released_on, presence: true
+	
+	validate :must_have_valid_trailer
+	def must_have_valid_trailer
+		unless trailer.include?('iframe')
+			errors.add.trailer(:trailer, 'must include iframe tag')
+		end
+	end
 end
